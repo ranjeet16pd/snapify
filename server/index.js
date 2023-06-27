cf52c580356c7adf6e65b0e6f5c132ca675bbaf6
ranjeet16pd <91504447+ranjeet16pd@users.noreply.github.com>
2023-06-28 00:14:14 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 
+
 app.use(cors());
 
 app.use(express.json())
